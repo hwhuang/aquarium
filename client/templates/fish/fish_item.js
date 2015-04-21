656321ecbe58458a522d103c8a54f06ddc0eabb1
hwhuang <hannahwhuang@gmail.com>
2015-04-21 15:39:50 -0700

    Each fish moves with user

--- client/templates/fish/fish_item.js
+++ client/templates/fish/fish_item.js
@@ -1,15 +1,16 @@
 //Moves the fish on arrow keys
 $(document).on('keyup', function(event) {
+  var userId = Meteor.userId();
   if (event.keyCode == '37') {
-    Fish.update(Fish.findOne()._id, {$inc: {left: -10}});
+    Fish.update(Fish.findOne({userId: userId})._id, {$inc: {left: -10}});
   }
   if (event.keyCode == '39') {
-    Fish.update(Fish.findOne()._id, {$inc: {left: 10}}); 
+    Fish.update(Fish.findOne({userId: userId})._id, {$inc: {left: 10}});
   }
   if (event.keyCode == '38') {
-    Fish.update(Fish.findOne()._id, {$inc: {top: -10}}); 
+    Fish.update(Fish.findOne({userId: userId})._id, {$inc: {top: -10}});
   } 
   if (event.keyCode == '40') {
-    Fish.update(Fish.findOne()._id, {$inc: {top: 10}}); 
+    Fish.update(Fish.findOne({userId: userId})._id, {$inc: {top: 10}});
   }
 });
