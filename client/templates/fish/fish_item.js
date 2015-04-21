b34591cdf32a3cb73385120929f11b3e34ebc597
hwhuang <hannahwhuang@gmail.com>
2015-04-21 15:45:09 -0700

    No errors when not logged in

--- client/templates/fish/fish_item.js
+++ client/templates/fish/fish_item.js
@@ -1,16 +1,16 @@
 //Moves the fish on arrow keys
 $(document).on('keyup', function(event) {
   var userId = Meteor.userId();
-  if (event.keyCode == '37') {
+  if (event.keyCode == '37' && userId) {
     Fish.update(Fish.findOne({userId: userId})._id, {$inc: {left: -10}});
   }
-  if (event.keyCode == '39') {
+  if (event.keyCode == '39' && userId) {
     Fish.update(Fish.findOne({userId: userId})._id, {$inc: {left: 10}});
   }
-  if (event.keyCode == '38') {
+  if (event.keyCode == '38' && userId) {
     Fish.update(Fish.findOne({userId: userId})._id, {$inc: {top: -10}});
   } 
-  if (event.keyCode == '40') {
+  if (event.keyCode == '40' && userId) {
     Fish.update(Fish.findOne({userId: userId})._id, {$inc: {top: 10}});
   }
 });
